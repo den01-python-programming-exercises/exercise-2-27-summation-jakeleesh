a608fa89b05bc5436babcc20e052465ee403403c
Jake Lee <jake.lee@fdmgroup.com>
2021-09-23 18:15:25 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #write your code below this line
+    num1 = int(input())
+    num2 = int(input())
+    num3 = int(input())
+    num4 = int(input())
+    answer = sum(num1, num2, num3, num4)
+    print("Sum: " + str(answer))
+
+def sum(num1, num2, num3, num4):
+    return num1 + num2 + num3 + num4
 
 if __name__ == '__main__':
     main()
